1cfe4559430dd7af4f8498a9dde2a6dddbc27ec8
Andrew Gusev <a.gusev@dataduck.com>
2024-01-18 12:32:36 +0300

    dependency injections

--- cmd/main.go
+++ cmd/main.go
@@ -3,13 +3,17 @@ package main
 import (
 	"github.com/mrandrew1/todo-app"
 	"github.com/mrandrew1/todo-app/pkg/handler"
+	"github.com/mrandrew1/todo-app/pkg/repository"
+	"github.com/mrandrew1/todo-app/pkg/service"
 	"log"
 )
 
 func main() {
-	handlers := new(handler.Handler)
-	srv := new(todo.Server)
+	repos := repository.NewRepository()
+	services := service.NewService(repos)
+	handlers := handler.NewHandler(services)
 
+	srv := new(todo.Server)
 	if err := srv.Run("8080", handlers.InitRoutes()); err != nil {
 		log.Fatalf("error running server: %v", err.Error())
 	}
